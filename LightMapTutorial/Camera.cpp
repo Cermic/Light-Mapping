0e72ed4962a97654400303149d4d850369a2ec01
Jack Smith <streamlined50@hotmail.com>
2021-11-20 04:08:44 +0000

    removed unneccesary files

--- LightMapTutorial/Camera.cpp
+++
@@ -1,8 +0,0 @@
-#include "Camera.h"
-		
-// Keeps the Camera position and rotation up to date
-void Camera::update(vec3 atPos, GLfloat rotation)
-{
-	at = atPos;
-	eye = util.moveForward(at, rotation, 1.0f);
-}
